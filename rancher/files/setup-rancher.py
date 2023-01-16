0c4037bcab5dca9bbfaaec134985cc489008e3a3
Harry James Hall <mission.deny.the.mission@gmail.com>
2023-01-16 14:57:40 +0000

    Rancher module works now, hopefully

--- rancher/files/setup-rancher.py
+++ rancher/files/setup-rancher.py
@@ -1,8 +1,29 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import requests
+import socket
+import os
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(B)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = (s.getsockname()[0])
+s.close()
+
+proxies = {
+    "http": None,
+    "https": None,
+}
+
+data = {"type":"setting","name":"telemetry.opt","value":"in"}
+response = requests.post("http://localhost:8080/v2-beta/setting", json = data, proxies=proxies)
+print(response)
+
+null = None
+false = False
+data = {"id":"api.host","type":"activeSetting","baseType":"setting","name":"api.host","activeValue":null,"inDb":false,"source":null,"value":"http://" + ip + ":8080"}
+response = requests.put("http://localhost:8080/v2-beta/settings/api.host", json=data, proxies=proxies)
+print(response)
+
+data = {"type":"registrationToken"}
+response = requests.post("http://localhost:8080/v2-beta/projects/1a5/registrationtoken", json=data, proxies=proxies)
+key = response.json()["uuid"]
+
+os.system("docker run -d --restart=unless-stopped --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.11 http://{}:8080/v1/scripts/{}".format(ip, key))
--- rancher/manifests/install.pp
+++ rancher/manifests/install.pp
@@ -5,7 +5,7 @@ class rancher::install {
       'https_proxy=http://172.22.0.51:3128',
       'ftp_proxy=http://172.22.0.51:3128'] }
 
-  ensure_packages(['apt-transport-https', 'ca-certificates', 'curl', 'gnupg2', 'software-properties-common', 'python3-pip'])
+  ensure_packages(['apt-transport-https', 'ca-certificates', 'curl', 'gnupg2', 'software-properties-common'])
 
   exec {'download-and-install-apt-key-for-docker-repo':
     command => 'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -',
@@ -20,9 +20,18 @@ class rancher::install {
     command => 'apt-get install -y docker-ce',
   } ->
   exec {'launch-rancher':
-    command => 'docker run -d --restart=unless-stopped -p 8080:8080 rancher/server',
+    command => 'docker stop $(docker ps -a -q); docker rm $(docker ps -qa); docker run -d --restart=unless-stopped -p 8080:8080 rancher/server',
   } ->
-  exec {'install-selenium':
-    command => 'pip install selenium',
+  exec {'sleep':
+    command => 'sleep 360',
+    timeout => 365,
+  } ->
+  file {'/root/setup-rancher.py':
+    source => 'puppet:///modules/rancher/setup-rancher.py',
+    mode => '0755',
+  } ->
+  exec {'configure-rancher':
+    command => 'python3 /root/setup-rancher-v2.py',
+    environment => [],
   }
 }
