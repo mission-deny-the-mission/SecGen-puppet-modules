a8ff7d1936e0ccedc393be40d6b64638e92ac9ab
Harry James Hall <mission.deny.the.mission@gmail.com>
2023-02-24 16:08:36 +0000

    Schedule configuring rancher for reboot

--- rancher/files/setup-rancher.py
+++ rancher/files/setup-rancher.py
@@ -3,35 +3,41 @@ import socket
 import os
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-ip = (s.getsockname()[0])
-s.close()
+def setup_rancher():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = (s.getsockname()[0])
+    s.close()
 
-proxies = {
-    "http": None,
-    "https": None,
-}
+    proxies = {
+        "http": None,
+        "https": None,
+    }
 
-data = {"type":"setting","name":"telemetry.opt","value":"in"}
-response = requests.post("http://localhost:8080/v2-beta/setting", json = data, proxies=proxies)
-print(response)
+    data = {"type":"setting","name":"telemetry.opt","value":"in"}
+    response = requests.post("http://localhost:8080/v2-beta/setting", json = data, proxies=proxies)
+    print(response)
 
-null = None
-false = False
-data = {"id":"api.host","type":"activeSetting","baseType":"setting","name":"api.host","activeValue":null,"inDb":false,"source":null,"value":"http://" + ip + ":8080"}
-response = requests.put("http://localhost:8080/v2-beta/settings/api.host", json=data, proxies=proxies)
-print(response)
+    null = None
+    false = False
+    data = {"id":"api.host","type":"activeSetting","baseType":"setting","name":"api.host","activeValue":null,"inDb":false,"source":null,"value":"http://" + ip + ":8080"}
+    response = requests.put("http://localhost:8080/v2-beta/settings/api.host", json=data, proxies=proxies)
+    print(response)
 
-data = {"type":"registrationToken"}
-response = requests.post("http://localhost:8080/v2-beta/projects/1a5/registrationtoken", json=data, proxies=proxies)
-#link = response.json()["actions"]["activate"]
-link = response.json()["links"]["self"]
-print(link)
+    data = {"type":"registrationToken"}
+    response = requests.post("http://localhost:8080/v2-beta/projects/1a5/registrationtoken", json=data, proxies=proxies)
+    #link = response.json()["actions"]["activate"]
+    link = response.json()["links"]["self"]
+    print(link)
 
-time.sleep(2)
+    time.sleep(2)
 
-response = requests.get(link, proxies=proxies)
-command = response.json()["command"]
-print(command)
-os.system(command)
+    response = requests.get(link, proxies=proxies)
+    command = response.json()["command"]
+    print(command)
+    os.system(command)
+
+if not os.path.exists("/root/ran"):
+    time.sleep(10)
+    setup_rancher()
+    os.system("touch /root/ran")
--- rancher/manifests/install.pp
+++ rancher/manifests/install.pp
@@ -5,9 +5,6 @@ class rancher::install {
 
   ensure_packages(['apt-transport-https', 'ca-certificates', 'curl', 'gnupg2', 'software-properties-common'])
 
-  exec {'remove-all-containers':
-    command => 'docker stop $(docker ps -a -q); docker rm $(docker ps -qa)'
-  } ->
   docker::run {'rancher-server':
     image => 'rancher/server',
     ports => ['8080:8080'],
@@ -17,19 +14,23 @@ class rancher::install {
     command => 'sleep 360',
     timeout => 365,
   } ->
+#  docker::image {'rancher/net': } ->
+#  docker::image {'rancher/network-manager': } ->
+#  docker::image {'rancher/dns': } ->
+#  docker::image {'rancher/healthcheck': } ->
+#  docker::image {'rancher/metadata': } ->
+#  docker::image {'rancher/scheduler': } ->
+#  docker::image {'rancher/agent': } ->
+  docker::image {'alpine': } ->
   file {'/root/setup-rancher.py':
     source => 'puppet:///modules/rancher/setup-rancher.py',
     mode => '0755',
   } ->
-  exec {'configure-rancher':
-    command => 'python3 /root/setup-rancher.py',
-    environment => [],
-  } ->
-  docker::image {'alpine':
-    image_tag => 'latest',
-  } ->
-  exec {'wait-for-rancher-host-agent':
-    command => 'sleep 60',
-    timeout => 65,
+#  exec {'configure-rancher':
+#    command => 'python3 /root/setup-rancher.py',
+#    environment => [],
+#  } ->
+  exec {'set-rancher-configuration-file-to-run-at-boot-via-crontab':
+    command => 'echo "@reboot root /usr/bin/python3 /root/setup-rancher.py" >> /etc/crontab; chmod 644 /etc/crontab'
   }
 }
